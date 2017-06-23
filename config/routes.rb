228d197af0a18ae1af731d1f9a43d55dabda67dc
Christine Tang <prismcolour@icloud.com>
2017-06-23 12:36:47 -0400

    Move root before user resources in routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+	root 'listings#index'
   devise_for :users
   resources :listings
   get 'pages/about'
   get 'pages/contact'
 
-  root 'listings#index'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
