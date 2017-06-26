5c6034b51308c1360ebc44f9d06c6c292c7dbea9
Christine Tang <prismcolour@icloud.com>
2017-06-25 22:45:02 -0400

    Add order history pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
 	
   devise_for :users
   resources :listings do
-  	resources :orders
+  	resources :orders, only: [:new, :create]
   end
 
   get 'pages/about'
   get 'pages/contact'
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
   
 
